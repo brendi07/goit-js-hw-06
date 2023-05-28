95c6e25f57caffed9db6618d85562acae5160ea2
brendi07 <taniasamoylenko45@gmail.com>
2023-05-28 16:57:58 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -12,7 +12,7 @@ function submitingForm(event) {
     if (data.email !== '' && data.password !== '') {
         console.log(data);
     } else if (data.email === '' || data.password === "") {
-        window.alert('Please fill in all the fields!');
+       return window.alert('Please fill in all the fields!');
     }
     event.currentTarget.reset();
 }
